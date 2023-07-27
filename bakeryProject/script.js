58350e9b41c41808a2df799e33832669a255fd51
katttaryna <77131930+katttaryna@users.noreply.github.com>
2023-07-27 20:18:25 +0300

    Add files via upload

---
+++ bakeryProject/script.js
@@ -0,0 +1,10 @@
+let btnOrder = document.querySelector('.btn-order')
+let btnAbout = document.querySelector('.btn-about')
+
+btnAbout.addEventListener('click', ()=>{
+    alert('Вся информация по номеру телефона')
+})
+
+btnOrder.addEventListener('click', ()=>{
+    alert('Чтобы оставить заказ заполните форму')
+})
